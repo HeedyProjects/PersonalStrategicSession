dd8d3fe8de9ce49892e1604e06bfbfc16f5bca61
reviakin dmitry <reviakindi@gmail.com>
2020-02-16 22:25:42 +0200

    add PrevSessions' screen to Main

--- src/navigation/Main/index.js
+++ src/navigation/Main/index.js
@@ -3,12 +3,14 @@ import {createSwitchNavigator} from 'react-navigation';
 import StartScreen from '../../features/StartScreen';
 import Questions from '../../features/Questions';
 import StepResult from '../../features/StepResult';
+import PrevSessions from '../../features/PrevSessions';
 
 const MainNavigator = createSwitchNavigator(
   {
     StartScreen: {screen: StartScreen},
     Questions: {screen: Questions},
     StepResult: {screen: StepResult},
+    PrevSessions: {screen: PrevSessions},
   },
   {
     initialRoute: 'StartScreen',
