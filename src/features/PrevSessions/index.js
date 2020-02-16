f9ced5f849593bbb3e53e086328e2f1e718a75b3
reviakin dmitry <reviakindi@gmail.com>
2020-02-16 22:46:52 +0200

    render previous sessions by date

--- src/features/PrevSessions/index.js
+++ src/features/PrevSessions/index.js
@@ -5,12 +5,31 @@ import {connect} from 'react-redux';
 import {styles} from './styles';
 import {RoundedButton} from '../../common/RoundedButton';
 
+/**
+ * Create the session's title from Date constr.
+ * @param {String} timeStr
+ * @returns {String}
+ */
+function makeSessionTitle(timeStr) {
+  const time = new Date(timeStr);
+  return `${time.getDate()} ${time.getMonth() + 1} ${time.getFullYear()}`;
+}
+
 function PrevSessions(props) {
   const {sessions, navigation} = props;
+  console.log(sessions);
   return (
     <View style={styles.container}>
       {sessions.length > 0 ? (
-        sessions.map((session, id) => <Text key={id}>{session.time}</Text>)
+        sessions.map((session, id) => (
+          <Text
+            style={styles.listItem}
+            key={id}
+            //! TODO: make navigation to the session screen.
+            onPress={() => navigation.navigate('SessionScreen')}>
+            {makeSessionTitle(session.time)}
+          </Text>
+        ))
       ) : (
         <Text>Sessions doesn't exist</Text>
       )}
