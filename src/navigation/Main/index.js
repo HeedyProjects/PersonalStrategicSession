4d9bdeef01654c48ccd07db7c0376beb7ac8f0f6
reviakin dmitry <reviakindi@gmail.com>
2020-02-23 18:50:32 +0200

    add SessionList, SessionScreen in MainNavigation

--- src/navigation/Main/index.js
+++ src/navigation/Main/index.js
@@ -3,14 +3,16 @@ import {createSwitchNavigator} from 'react-navigation';
 import StartScreen from '../../features/StartScreen';
 import Questions from '../../features/Questions';
 import StepResult from '../../features/StepResult';
-import PrevSessions from '../../features/PrevSessions';
+import SessionsList from '../../features/SessionsList';
+import SessionScreen from '../../features/SessionScreen';
 
 const MainNavigator = createSwitchNavigator(
   {
     StartScreen: {screen: StartScreen},
     Questions: {screen: Questions},
     StepResult: {screen: StepResult},
-    PrevSessions: {screen: PrevSessions},
+    SessionsList: {screen: SessionsList},
+    SessionScreen: {screen: SessionScreen},
   },
   {
     initialRoute: 'StartScreen',
